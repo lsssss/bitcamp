2ff77c0d26962b2f5ec0dc13974faa6e7ca9ec78
lssss <vkqqnfl@naver.com>
2018-01-04 17:48:53 +0900

    '20180104'

---
+++ java-project/src68/main/java/java100/app/dao/ScoreDao.java
@@ -0,0 +1,34 @@
+package java100.app.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import java100.app.domain.Score;
+
+// ScoreDao 클래스로부터 메서드 호출 규칙만 분리하여 따로 정의한다.
+//=> setDataSource()는 컨트롤러가 호출하는 메서드가 아니다.
+//=> 그래서 규칙에서 제외했다.
+//=> 이 규칙은 ScoreController가 호출하는 규칙이다.
+//
+public interface ScoreDao {
+    List<Score> findAll(Map<String,Object> params);
+    int insert(Score score);
+    int update(Score score);
+    int delete(int no);
+    Score findByNo(int no);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ java-project/src68/main/webapp/WEB-INF/jsp/footer.jsp
@@ -0,0 +1,11 @@
+<%@page import="java100.app.domain.Score"%>
+<%@page import="java100.app.dao.ScoreDao"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8" trimDirectiveWhitespaces="true"%>
+
+<footer> 비트캠프 자바100기 </footer>
+
+
+  
+  
+  
